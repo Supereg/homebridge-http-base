3e193489189592ba5531a09e22f6a555c852c539
Supereg <mail@anderl-bauer.de>
2019-07-25 12:31:39 +0200

    Code cleanup inside logger.js

--- test/logger.js
+++ test/logger.js
@@ -1,7 +1,7 @@
-var chalk = require('chalk');
-var util = require('util');
-
 'use strict';
+
+const chalk = require('chalk');
+const util = require('util');
 
 module.exports = {
     Logger: Logger,
@@ -9,10 +9,10 @@ module.exports = {
     setTimestampEnabled: setTimestampEnabled,
     forceColor: forceColor,
     _system: new Logger() // system logger, for internal use only
-}
+};
 
-var DEBUG_ENABLED = false;
-var TIMESTAMP_ENABLED = true;
+let DEBUG_ENABLED = false;
+let TIMESTAMP_ENABLED = true;
 
 // Turns on debug level logging
 function setDebugEnabled(enabled) {
@@ -31,7 +31,7 @@ function forceColor() {
 }
 
 // global cache of logger instances by plugin name
-var loggerCache = {};
+const loggerCache = {};
 
 /**
  * Logger class
@@ -44,33 +44,33 @@ function Logger(prefix) {
 Logger.prototype.debug = function(msg) {
     if (DEBUG_ENABLED)
         this.log.apply(this, ['debug'].concat(Array.prototype.slice.call(arguments)));
-}
+};
 
 Logger.prototype.info = function(msg) {
     this.log.apply(this, ['info'].concat(Array.prototype.slice.call(arguments)));
-}
+};
 
 Logger.prototype.warn = function(msg) {
     this.log.apply(this, ['warn'].concat(Array.prototype.slice.call(arguments)));
-}
+};
 
 Logger.prototype.error = function(msg) {
     this.log.apply(this, ['error'].concat(Array.prototype.slice.call(arguments)));
-}
+};
 
 Logger.prototype.log = function(level, msg) {
 
     msg = util.format.apply(util, Array.prototype.slice.call(arguments, 1));
-    func = console.log;
+    let func = console.log;
 
-    if (level == 'debug') {
+    if (level === 'debug') {
         msg = chalk.gray(msg);
     }
-    else if (level == 'warn') {
+    else if (level === 'warn') {
         msg = chalk.yellow(msg);
         func = console.error;
     }
-    else if (level == 'error') {
+    else if (level === 'error') {
         msg = chalk.bold.red(msg);
         func = console.error;
     }
@@ -81,20 +81,20 @@ Logger.prototype.log = function(level, msg) {
 
     // prepend timestamp
     if (TIMESTAMP_ENABLED) {
-        var date = new Date();
+        const date = new Date();
         msg =  chalk.white("[" + date.toLocaleString() + "]") + " " + msg;
     }
 
     func(msg);
-}
+};
 
 Logger.withPrefix = function(prefix) {
 
     if (!loggerCache[prefix]) {
         // create a class-like logger thing that acts as a function as well
         // as an instance of Logger.
-        var logger = new Logger(prefix);
-        var log = logger.info.bind(logger);
+        const logger = new Logger(prefix);
+        const log = logger.info.bind(logger);
         log.debug = logger.debug;
         log.info = logger.info;
         log.warn = logger.warn;
@@ -105,4 +105,4 @@ Logger.withPrefix = function(prefix) {
     }
 
     return loggerCache[prefix];
-}
+};
